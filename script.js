998fef477b83b8c8e57e9473b61ff71a180ead85
NewHellequin <tom.mightysloth.mills@gmail.com>
2023-09-17 20:21:21 +0100

    Addition of 'Highscore' tracking feature

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -26,10 +27,13 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
     document.querySelector('.number').textContent = secretNumber;
-
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
@@ -66,5 +70,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.guess').value = '';
   document.querySelector('.number').textContent = '?';
   document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.number').style.backgroundColor = '#222';
+  document.querySelector('body').style.backgroundColor = '#222';
 });
